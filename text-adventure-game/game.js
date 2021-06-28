bf3dd064b51e70999bbcd7b4e05b738205c331c4
Eric Fan <fanxiaoyuan2015@outlook.com>
2021-06-28 14:10:45 -0400

    text game 2

--- text-adventure-game/game.js
+++ text-adventure-game/game.js
@@ -56,7 +56,7 @@ const textNodes = [
     },
     {
         id: 2,
-        text: 'You feel pain piercing through your right thigh as you attempt to get up...',
+        text: 'As you attempt to get up, burning pain pierced through your right thigh...',
         options: [
             {
                 text: 'Let the Magpie heal your wound',
